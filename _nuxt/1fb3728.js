13645f65f878e4e38372c073f2b49a6185eaeb13
tiron <tiron@MacBook-Pro-tiron.local>
2022-07-25 20:35:03 +0300

    1cc2c24

---
+++ _nuxt/1fb3728.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{296:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},o=t(61),component=Object(o.a)(l,(function(){return(0,this._self._c)("Main")}),[],!1,null,null,null);e.default=component.exports;installComponents(component,{Main:t(295).default})}}]);
